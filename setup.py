e32efab7a993e4996d96583bc3c3e942bc235cbd
Stijn Rosaer <stijn.rosaer@student.uantwerpen.be>
2021-07-30 10:00:46 +0200

    update package information

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SPARQL-helper",
+    name="mu-SPARQL-helpers",
     version="0.0.1",
     author="Stijn Rosaer",
     author_email="stijn.rosaer@telenet.be",
-    description="Python helper for SPARQL",
+    description="Python helper for SPARQL for a mu.semte.ch stack",
     long_description=long_description,
     python_requires=">=3.6",
     install_requires=[
@@ -17,8 +17,9 @@ setuptools.setup(
         "SPARQLWrapper",
         "rdflib"
     ],
-    url="https://github.com/stijnrosaer/job-run-loop",
+    url="https://github.com/stijnrosaer/mu-python-sparql-helper",
     packages=setuptools.find_packages(),
+    keywords = ["mu", "sparql"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
