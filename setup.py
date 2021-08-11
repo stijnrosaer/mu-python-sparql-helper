7d4916c1e025047478ee66fcfa548b11e1c80b36
Stijn Rosaer <stijn.rosaer@student.uantwerpen.be>
2021-08-11 14:45:50 +0200

    change package name and add description content type

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mu-SPARQL-helpers",
-    version="0.0.1",
+    name="mu-sparql-helpers",
+    version="0.1",
     author="Stijn Rosaer",
     author_email="stijn.rosaer@telenet.be",
     description="Python helper for SPARQL for a mu.semte.ch stack",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.6",
     install_requires=[
         "flask",
